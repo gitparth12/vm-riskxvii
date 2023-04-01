d4a04c9e79e82483d549f4b9ce8a5cea16622b2f
Parth Bhargava <pbha6036@uni.sydney.edu.au>
2023-04-01 17:41:29 +1100

    submission

---
+++ vm_riskxvii.h
@@ -0,0 +1,27 @@
+#ifndef VM_RISKXVII
+#define VM_RISKXVII
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define INST_MEM_SIZE (256)
+#define DATA_MEM_SIZE (1024)
+#define INST_BYTES (4)
+#define OPCODE_LENGTH (7)
+#define FUNC3_START (12)
+#define FUNC7_START (25)
+
+typedef struct blob {
+    // Instruction Memory
+    uint8_t inst_mem[DATA_MEM_SIZE];
+    // Data memory
+    uint8_t data_mem[DATA_MEM_SIZE];
+    // Registers for the vm
+    int32_t registers[32];
+    // Program counter
+    uint32_t PC;
+} blob;
+
+#endif
