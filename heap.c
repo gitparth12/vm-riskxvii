8c242f109e0b7fcd684e9ea5220ee13896a92ed4
Parth Bhargava <pbha6036@uni.sydney.edu.au>
2023-04-02 19:26:15 +1000

    submission

--- heap.c
+++ heap.c
@@ -28,16 +28,16 @@ uint32_t my_malloc(blob* p_vm, uint32_t malloc_size) {
                 new->next = NULL;
                 new->size = malloc_size;
                 new->start_address = current->start_address + (current->num_banks * 64);
-                printf("current_start: %x, start: %x, size: %d, num_banks: %d\n", current->start_address, new->start_address, new->size, new->num_banks);
+                printf("IF CONDITION: current_start: %x, start: %x, size: %d, num_banks: %d\n", current->start_address, new->start_address, new->size, new->num_banks);
                 return new->start_address;
             }
             else if (current->next == NULL && (current->start_address + current->size + malloc_size) > 0xd700) {
                 return 0; // If there's no space to malloc at the end of the list
             }
             else {
-                uint32_t end_address = current->start_address + current->size;
+                uint32_t end_address = current->start_address + (current->num_banks * 64);
                 uint32_t next_address = current->next->start_address;
-                if ((next_address - end_address) >= malloc_size) {
+                if ((next_address - end_address) >= malloc_size && (next_address - end_address) >= 64) {
                     // Insert a node in the list here
                     Node* new = (Node*) malloc(1 * sizeof(Node));
                     new->p_data = (int8_t*) calloc(malloc_size, sizeof(uint8_t));
@@ -46,7 +46,7 @@ uint32_t my_malloc(blob* p_vm, uint32_t malloc_size) {
                     new->size = malloc_size;
                     new->start_address = current->start_address + (current->num_banks * 64);
                     current->next = new;
-                    //printf("current_start: %x, start: %x, size: %d, num_banks: %d\n", current->start_address, new->start_address, new->size, new->num_banks);
+                    printf("ELSE CONDITION: current_start: %x, start: %x, size: %d, num_banks: %d\n", current->start_address, new->start_address, new->size, new->num_banks);
                     return new->start_address;
                 }
             }
