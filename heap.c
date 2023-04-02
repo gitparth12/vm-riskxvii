d3a22e9cb7a1a8fa90fb4339b0ab393b6880e90b
Parth Bhargava <pbha6036@uni.sydney.edu.au>
2023-04-02 22:47:22 +1000

    submission

--- heap.c
+++ heap.c
@@ -183,7 +183,7 @@ void lbu_h(blob* p_vm, uint32_t reg_index, uint32_t address) {
     while (current != NULL) {
         if (address >= current->start_address && (address+3) <= (current->start_address + current->size)) {
             address -= current->start_address;
-            p_vm->registers[reg_index] = (uint32_t) current->p_data[address];
+            p_vm->registers[reg_index] = (uint8_t) current->p_data[address];
             return;
         }
         current = current->next;
