85ff7fe64a4485864cdff5a3c265ec38e4722706
Parth Bhargava <pbha6036@uni.sydney.edu.au>
2023-04-02 18:51:55 +1000

    submission

--- heap.c
+++ heap.c
@@ -13,6 +13,7 @@ uint32_t my_malloc(blob* p_vm, uint32_t malloc_size) {
         temp->next = NULL;
         temp->size = malloc_size;
         temp->start_address = 0xb700;
+        printf("start: %x, size: %d, num_banks: %d\n", temp->start_address, temp->size, temp->num_banks);
         return temp->start_address;
     }
     else {
@@ -27,6 +28,7 @@ uint32_t my_malloc(blob* p_vm, uint32_t malloc_size) {
                 new->next = NULL;
                 new->size = malloc_size;
                 new->start_address = current->start_address + current->size + 1;
+                printf("start: %x, size: %d, num_banks: %d\n", new->start_address, new->size, new->num_banks);
                 return new->start_address;
             }
             else if (current->next == NULL && (current->start_address + current->size + malloc_size) > 0xd700) {
@@ -44,6 +46,7 @@ uint32_t my_malloc(blob* p_vm, uint32_t malloc_size) {
                     new->size = malloc_size;
                     new->start_address = current->start_address + current->size + 1;
                     current->next = new;
+                    printf("start: %x, size: %d, num_banks: %d\n", new->start_address, new->size, new->num_banks);
                     return new->start_address;
                 }
             }
